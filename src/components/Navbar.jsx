cd574a6fbc5bed4719598a7a02e3f745c139a431
zone2fun <zone2fun@gmail.com>
2025-10-16 20:39:25 +0700

    Change register button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -75,7 +75,7 @@ const Navbar = () => {
                        }
                                   
                 </div> 
-                : <button onClick={()=> navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
+                : <button onClick={()=> navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>สมัครสมาชิก</button>
             }
 
             <img onClick={()=>setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
